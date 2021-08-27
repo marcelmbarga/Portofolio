b3b78ce29e399469af2f8a4dcf18db2da580e811
Mbarga <juniormbarga@yahoo.fr>
2021-08-27 12:09:44 +0900

    airbnbish

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,6 +10,16 @@ export const projects = [
     id: 0,
   },
   {
+    title: "Airbnb-ish",
+    description:
+      "Airbnb UI clone using Next.js + tailwind css. ( Search functionality and Mapbox integration).",
+    image: "/images/airbnb_build.png",
+    tags: ["Next", "Tailwind", "Mapbox"],
+    source: "https://github.com/marcelmbarga/airbnb_ish",
+    visit: "https://airbnb-ish-gamma.vercel.app/",
+    id: 1,
+  },
+  {
     title: "Fake Netflix",
     description:
       "This is Netflix clone build! A React project with multiple pages: Home, Browse (which uses Firebase authentication), sign in, and sign up. The sign in will connect to Firebase when a user tries to sign in, and when a user signs up, we will use Firebase auth to store the user in the Firebase auth database.",
@@ -17,7 +27,7 @@ export const projects = [
     tags: ["React", "Redux", "Firebase", "Stripe"],
     source: "https://github.com/marcelmbarga/Fake-Netflix",
     visit: "https://fake-netflix-build.web.app/",
-    id: 1,
+    id: 2,
   },
   {
     title: "disneyplus-clone",
@@ -27,7 +37,7 @@ export const projects = [
     tags: ["React", "Redux", "Firebase", "Styled Components"],
     source: "https://github.com/marcelmbarga/DisneyPlus-Clone",
     visit: "https://disneyplus-clone-7c64f.web.app/",
-    id: 2,
+    id: 3,
   },
   {
     title: "Face Detection",
@@ -37,7 +47,7 @@ export const projects = [
     tags: ["React", "Express", "RestAPI", "PostgreSQL"],
     source: "https://github.com/marcelmbarga/face-recognition-brain",
     visit: "https://smart-brain86.herokuapp.com/",
-    id: 3,
+    id: 4,
   },
 ];
 
